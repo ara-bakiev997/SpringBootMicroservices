0003f7d7d338c68a1eb7e42c57ac8fafc9bc7231
Бакиев Айрат Иршатович <AIBakiev@sberbank.ru>
2023-12-14 09:02:23 +0300

    product-service: testcontainers

--- product-service/src/test/java/edu/microservices/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/edu/microservices/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,37 @@
 package edu.microservices.productservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo"));
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @DynamicPropertySource
+    static void setProperties(final DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @Test
+    void shouldCreateProduct() {
+//        mockMvc.perform(MockMvcBuilder.)
+    }
 
 }
