1a970822ad316d188e8a59d8a91738f43dfb124e
Бакиев Айрат Иршатович <AIBakiev@sberbank.ru>
2023-12-16 22:14:36 +0300

    spring-boot-microservices: create parent pom.xml, migrate services to parent module, using one docker-compose file

--- order-service/src/main/java/edu/microservices/orderservice/service/OrderService.java
+++ order-service/src/main/java/edu/microservices/orderservice/service/OrderService.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class OrderService {
     private final OrderRepository orderRepository;
 
